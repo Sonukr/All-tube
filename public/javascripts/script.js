c6956ed9308f83aeb839d385c42ce1cb1470320d
sonu <sonuyadav9756@gmail.com>
2015-08-02 00:03:51 +0530

    secure https

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,7 @@ var app = angular.module('Alltube', []);
 
 
 app.controller('MainCtrl', function($scope, $http) {
-var url = 'http://api-videos.herokuapp.com/youtube?query=humnawa';
+var url = 'https://api-videos.herokuapp.com/youtube?query=humnawa';
     $http.get(url)
          .success(function(data){
      $scope.videos = data.videos;
