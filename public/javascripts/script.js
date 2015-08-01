0a44661ebc706ffaba1e36e9265506cb9f2940db
sonu <sonuyadav9756@gmail.com>
2015-08-02 02:53:14 +0530

    Basic Working module ready

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,12 +2,56 @@
 var app = angular.module('Alltube', []);
 
 
-app.controller('MainCtrl', function($scope, $http) {
-var url = 'https://api-videos.herokuapp.com/youtube?query=humnawa';
-    $http.get(url)
-         .success(function(data){
-     $scope.videos = data.videos;
- console.log(data)
-});
+app.controller('MainCtrl', function($scope, $http, $sce) {
+    $scope.video_url = $sce.trustAsResourceUrl("https://www.youtube.com/embed/wuV4BCYv-YY?rel=0&showinfo=0&modestbranding=1&autohide=1&theme=light&hd=1");
+
+    var url = 'https://api-videos.herokuapp.com/youtube';
+        $http.get(url)
+             .success(function(data){
+             $scope.videos = data.videos;
+             $scope.page = data.page;
+            });
+    $scope.searchvideo = function (evt) {
+        var query = $scope.query;
+        console.log(query)
+        var url = 'https://api-videos.herokuapp.com/youtube?query=' + query;
+        $http.get(url)
+            .success(function (data) {
+                $scope.videos = data.videos;
+
+            });
+    };
+
+    $scope.play_video = function(url){
+        var url_id = "https://www.youtube.com/embed/" + url+"?rel=0&showinfo=0&modestbranding=1&autohide=1&theme=light&hd=1";
+        $scope.video_url = $sce.trustAsResourceUrl(url_id);
+    };
+
+    $scope.search_by_paging = function(page_link){
+
+        var url = 'https://api-videos.herokuapp.com/youtube/page?page=' + page_link;
+        console.log(url)
+        $http.get(url)
+            .success(function (data) {
+                $scope.videos = data.videos;
+
+            });
+
+    };
 
 })
+
+
+//===============================================================
+
+
+$(document).ready(function(){
+    $('input[type=text]').keypress(function(event){
+        if(event.which == 13){
+            $("#search").click();
+            //Prevent the action on the element
+            event.preventDefault();
+        }
+    });
+
+});
