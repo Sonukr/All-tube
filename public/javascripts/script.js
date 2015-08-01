b68bf1576847d4d38eabd8c09052717b3139afe1
sonu <sonuyadav9756@gmail.com>
2015-08-01 23:48:44 +0530

    setup with api

---
+++ public/javascripts/script.js
@@ -0,0 +1,13 @@
+
+var app = angular.module('Alltube', []);
+
+
+app.controller('MainCtrl', function($scope, $http) {
+var url = 'http://api-videos.herokuapp.com/youtube?query=humnawa';
+    $http.get(url)
+         .success(function(data){
+     $scope.videos = data.videos;
+ console.log(data)
+});
+
+})
