f01a0a1e64970a932dd949d97b9f83a5d88a840c
Sonu kumar <sonuyadav9756@gmail.com>
2016-02-21 19:02:37 +0530

    routing added for videos , and choice enabled to play video on same page or overy every video page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,46 @@
 
-var app = angular.module('Alltube', []);
+var app = angular.module('Alltube', ['ngRoute']);
 
+    app.config(function($routeProvider){
+        $routeProvider.when('/',{
+           templateUrl:'/video.html',
+            controller:'MainCtrl'
+        });
+
+        $routeProvider.when('/video/:id',{
+            templateUrl:'/play_video.html',
+            controller:'play'
+        });
+
+    });
+
+
+// for play video over his own url. :)
+
+app.controller('play', function ($scope, $routeParams, $sce) {
+        //Get ID out of current URL
+        var currentId = $routeParams.id;
+        $("#main").fitVids();
+        $scope.video_url = $sce.trustAsResourceUrl("https://www.youtube.com/embed/aGe9Hr_9YEU?autoplay=0&rel=0&showinfo=0&modestbranding=1&autohide=1&hd=1");
+        var url_id = "https://www.youtube.com/embed/" + currentId+"?autoplay=1&rel=0&showinfo=0&modestbranding=1&autohide=1&hd=1";
+        $scope.video_url = $sce.trustAsResourceUrl(url_id);
+        $("#main").fitVids();
+
+});
 
 app.controller('MainCtrl', function($scope, $http, $sce) {
+
+    $scope.name = " ";
+
     $scope.video_url = $sce.trustAsResourceUrl("https://www.youtube.com/embed/wuV4BCYv-YY?autoplay=0&rel=0&showinfo=0&modestbranding=1&autohide=1&hd=1");
 
-
     var url = 'https://api-videos.herokuapp.com/';
-        $http.get(url)
-             .success(function(data){
-             $scope.videos = data.videos;
-             $scope.page = data.page;
-            });
+
+    $http.get(url).success(function(data){
+         $scope.videos = data.videos;
+         $scope.page = data.page;
+        });
+
     $scope.search_video = function (evt) {
         $(".loading").fadeIn(10);
         var query = $scope.query;
@@ -20,13 +49,16 @@ app.controller('MainCtrl', function($scope, $http, $sce) {
             .success(function (data) {
                 $scope.videos = data.videos;
                 $scope.page = data.page;
+                console.log($scope.videos);
                 $(".loading").fadeOut(100);
             });
     };
 
-    $scope.play_video = function(url){
-        var url_id = "https://www.youtube.com/embed/" + url+"?autoplay=1&rel=0&showinfo=0&modestbranding=1&autohide=1&hd=1";
+    $scope.play_video = function(video){
+        var embed_url = video.embed_url;
+        var url_id = "https://www.youtube.com/embed/" + embed_url+"?autoplay=1&rel=0&showinfo=0&modestbranding=1&autohide=1&hd=1";
         $scope.video_url = $sce.trustAsResourceUrl(url_id);
+        $scope.name = video.name;
         $("html, body").animate({ scrollTop: 0 }, 1000);
     };
 
@@ -62,13 +94,13 @@ app.controller('MainCtrl', function($scope, $http, $sce) {
 
 $(window).load(function(){
     $(".loading").fadeOut(100);
+    $("#main").fitVids();
 });
 
 $(document).ready(function(){
     $('.dropdown-toggle').dropdown();
     //for fitvid
-    $("#main").fitVids();
-    //$(".loading").fadeOut(100);
+
 
     $('input[type=text]').keypress(function(event){
         if(event.which == 13){
